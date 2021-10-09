b19b032fa2e6de331c5b16a2c5ed7b494330fe47
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2021-10-10 01:01:18 +0200

    Add open with file

--- src/main/java/origami_editor/editor/OrigamiEditor.java
+++ src/main/java/origami_editor/editor/OrigamiEditor.java
@@ -3,13 +3,19 @@ package origami_editor.editor;
 import com.formdev.flatlaf.FlatLightLaf;
 
 import javax.swing.*;
+import java.io.File;
 
 public class OrigamiEditor {
     public static void main(String[] argv) {
         SwingUtilities.invokeLater(() -> {
             FlatLightLaf.setup();
 
-            new App();//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Rewrite location
+            App app = new App();//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Rewrite location
+
+            if (argv.length == 1) {
+                // We got a file
+                app.openFile(new File(argv[0]));
+            }
         });
     }
 }
