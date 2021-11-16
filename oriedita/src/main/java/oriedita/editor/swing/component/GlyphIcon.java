1873ed15ba3bac49c23299be1c302c5cd864f5b7
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2021-11-16 18:20:23 +0100

    Reset font after drawing glyph

--- oriedita/src/main/java/oriedita/editor/swing/component/GlyphIcon.java
+++ oriedita/src/main/java/oriedita/editor/swing/component/GlyphIcon.java
@@ -23,10 +23,12 @@ public class GlyphIcon implements Icon, PropertyChangeListener {
     @Override
     public void paintIcon(Component c, Graphics g, int x, int y) {
         Graphics2D g2 = (Graphics2D) g;
+        Font font = g2.getFont();
         g2.setFont(new Font("Icons", Font.PLAIN, 20));
         g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_GASP);
         g2.setColor(Colors.get(color));
         g2.drawString(glyph, x, y + 20);
+        g2.setFont(font);
     }
 
     @Override
@@ -43,9 +45,6 @@ public class GlyphIcon implements Icon, PropertyChangeListener {
     public void propertyChange(PropertyChangeEvent evt) {
         if (evt.getPropertyName() != null && evt.getPropertyName().equals("foreground")) {
             color = (Color) evt.getNewValue();
-
-//            AbstractButton btn = (AbstractButton) evt.getSource();
-//            btn.repaint();
         }
     }
 }
