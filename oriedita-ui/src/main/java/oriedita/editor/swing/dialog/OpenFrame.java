939da24dff3bda1b2591d4eabe5307672cc2c055
Minh Than <hoangminh117@gmail.com>
2023-03-04 18:23:17 -0600

    Migrate the replace tool to the left panel

--- oriedita-ui/src/main/java/oriedita/editor/swing/dialog/OpenFrame.java
+++ oriedita-ui/src/main/java/oriedita/editor/swing/dialog/OpenFrame.java
@@ -6,20 +6,14 @@ import oriedita.editor.canvas.CreasePattern_Worker;
 import oriedita.editor.canvas.MouseMode;
 import oriedita.editor.databinding.ApplicationModel;
 import oriedita.editor.databinding.CanvasModel;
-import oriedita.editor.handler.CustomLineTypes;
 import oriedita.editor.service.ButtonService;
-
-import javax.swing.DefaultComboBoxModel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JDialog;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -37,18 +31,11 @@ public class OpenFrame extends JDialog {
     private JButton del_lButton;
     private JButton del_l_XButton;
     private JPanel panel;
-    private JButton replace_lineButton;
-    private JComboBox<String> fromLineDropBox;
-    private JComboBox<String> toLineDropBox;
-
-    private CustomLineTypes customLineTypes;
-
     private ApplicationModel applicationModel;
 
     public OpenFrame(String name, Frame owner, CanvasModel canvasModel, CreasePattern_Worker mainCreasePatternWorker, ButtonService buttonService, ApplicationModel applicationModel) {
         super(owner, name);
         this.applicationModel = applicationModel;
-        setData(applicationModel);
 
         setContentPane($$$getRootComponent$$$());
 
@@ -63,7 +50,6 @@ public class OpenFrame extends JDialog {
         buttonService.registerButton(del_lButton, "del_lAction");
         buttonService.registerButton(del_l_XButton, "del_l_XAction");
         buttonService.registerButton(selectAnd3ClickCheckBox, "selectAnd3ClickAction");
-        buttonService.registerButton(replace_lineButton, "replace_lineButton");
 
         o_F_checkButton.addActionListener(e -> canvasModel.setMouseMode(MouseMode.FLAT_FOLDABLE_CHECK_63));
         foldableLinePlusGridInputButton.addActionListener(e -> {
@@ -78,18 +64,6 @@ public class OpenFrame extends JDialog {
         unselect_lXButton.addActionListener(e -> canvasModel.setMouseMode(MouseMode.UNSELECT_LINE_INTERSECTING_69));
         del_lButton.addActionListener(e -> canvasModel.setMouseMode(MouseMode.CREASE_DELETE_OVERLAPPING_64));
         del_l_XButton.addActionListener(e -> canvasModel.setMouseMode(MouseMode.CREASE_DELETE_INTERSECTING_65));
-        replace_lineButton.addActionListener(e -> {
-            canvasModel.setMouseMode(MouseMode.REPLACE_LINE_TYPE_SELECT_72);
-            mainCreasePatternWorker.unselect_all(false);
-        });
-        fromLineDropBox.addActionListener(e -> {
-            applicationModel.setCustomFromLineType(CustomLineTypes.from(fromLineDropBox.getSelectedIndex() - 1));
-            fromLineDropBox.setSelectedIndex(applicationModel.getCustomFromLineType().getType() + 1);
-        });
-        toLineDropBox.addActionListener(e -> {
-            applicationModel.setCustomToLineType(CustomLineTypes.from(toLineDropBox.getSelectedIndex()));
-            toLineDropBox.setSelectedIndex(applicationModel.getCustomToLineType().getType());
-        });
 
         selectAnd3ClickCheckBox.addActionListener(e -> canvasModel.setCkbox_add_frame_SelectAnd3click_isSelected(selectAnd3ClickCheckBox.isSelected()));
 
@@ -110,15 +84,9 @@ public class OpenFrame extends JDialog {
             unselect_lXButton.setSelected(m == MouseMode.UNSELECT_LINE_INTERSECTING_69);
             del_lButton.setSelected(m == MouseMode.CREASE_DELETE_OVERLAPPING_64);
             del_l_XButton.setSelected(m == MouseMode.CREASE_DELETE_INTERSECTING_65);
-            replace_lineButton.setSelected(m == MouseMode.REPLACE_LINE_TYPE_SELECT_72);
         }
 
         selectAnd3ClickCheckBox.setSelected(data.isCkbox_add_frame_SelectAnd3click_isSelected());
-    }
-
-    public void setData(ApplicationModel applicationModel) {
-        fromLineDropBox.setSelectedIndex(applicationModel.getCustomFromLineType().getType() + 1);
-        toLineDropBox.setSelectedIndex(applicationModel.getCustomToLineType().getType());
     }
 
     {
@@ -182,53 +150,6 @@ public class OpenFrame extends JDialog {
         gbc.gridwidth = 2;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         panel1.add(del_lButton, gbc);
-        replace_lineButton = new JButton();
-        replace_lineButton.setMaximumSize(new Dimension(-1, -1));
-        replace_lineButton.setMinimumSize(new Dimension(-1, -1));
-        replace_lineButton.setText("replace_line");
-        gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 4;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        panel1.add(replace_lineButton, gbc);
-        fromLineDropBox = new JComboBox();
-        fromLineDropBox.setMaximumSize(new Dimension(-1, -1));
-        fromLineDropBox.setMinimumSize(new Dimension(-1, -1));
-        final DefaultComboBoxModel defaultComboBoxModel1 = new DefaultComboBoxModel();
-        defaultComboBoxModel1.addElement("Any");
-        defaultComboBoxModel1.addElement("Edge");
-        defaultComboBoxModel1.addElement("Mountain");
-        defaultComboBoxModel1.addElement("Valley");
-        defaultComboBoxModel1.addElement("Aux");
-        fromLineDropBox.setModel(defaultComboBoxModel1);
-        gbc = new GridBagConstraints();
-        gbc.gridx = 1;
-        gbc.gridy = 4;
-        gbc.anchor = GridBagConstraints.WEST;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        panel1.add(fromLineDropBox, gbc);
-        final JLabel label1 = new JLabel();
-        label1.setText(" -> ");
-        gbc = new GridBagConstraints();
-        gbc.gridx = 2;
-        gbc.gridy = 4;
-        gbc.anchor = GridBagConstraints.WEST;
-        panel1.add(label1, gbc);
-        toLineDropBox = new JComboBox();
-        toLineDropBox.setMaximumSize(new Dimension(-1, -1));
-        toLineDropBox.setMinimumSize(new Dimension(-1, -1));
-        final DefaultComboBoxModel defaultComboBoxModel2 = new DefaultComboBoxModel();
-        defaultComboBoxModel2.addElement("Edge");
-        defaultComboBoxModel2.addElement("Mountain");
-        defaultComboBoxModel2.addElement("Valley");
-        defaultComboBoxModel2.addElement("Aux");
-        toLineDropBox.setModel(defaultComboBoxModel2);
-        gbc = new GridBagConstraints();
-        gbc.gridx = 3;
-        gbc.gridy = 4;
-        gbc.anchor = GridBagConstraints.WEST;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        panel1.add(toLineDropBox, gbc);
         unselect_polygonButton = new JButton();
         unselect_polygonButton.setBackground(new Color(-16711936));
         unselect_polygonButton.setText("unselect_polygon");
