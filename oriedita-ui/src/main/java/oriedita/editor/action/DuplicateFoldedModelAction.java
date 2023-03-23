fecf67f2eede639ad6864f6fd892c7ca2039258a
Minh Than <hoangminh117@gmail.com>
2023-03-22 20:07:02 -0500

    Fix duplicate not working for 2 colored CP

--- oriedita-ui/src/main/java/oriedita/editor/action/DuplicateFoldedModelAction.java
+++ oriedita-ui/src/main/java/oriedita/editor/action/DuplicateFoldedModelAction.java
@@ -2,10 +2,13 @@ package oriedita.editor.action;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import oriedita.editor.Foldable;
 import oriedita.editor.databinding.FoldedFiguresList;
+import oriedita.editor.drawing.tools.Camera;
 import oriedita.editor.service.FoldingService;
+import origami.crease_pattern.LineSegmentSet;
 import origami.folding.FoldedFigure;
-
 import java.awt.event.ActionEvent;
 
 @ApplicationScoped
@@ -16,10 +19,23 @@ public class DuplicateFoldedModelAction extends AbstractOrieditaAction {
     @Inject
     FoldedFiguresList foldedFiguresList;
     @Inject
+    @Named("creasePatternCamera")
+    Camera creasePatternCamera;
+    @Inject
     public DuplicateFoldedModelAction(){}
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        foldingService.duplicate(foldedFiguresList.getActiveItem().getFoldedFigure());
+        if(foldedFiguresList.getActiveItem().getFoldedFigure().estimationStep != FoldedFigure.EstimationStep.STEP_10){
+            foldingService.duplicate(foldedFiguresList.getActiveItem().getFoldedFigure());
+        } else {
+            LineSegmentSet lines = foldedFiguresList.getActiveItem().getFoldedFigure().wireFrame_worker1.getLineStore();
+            Foldable newFigure = foldingService.initFoldedFigure();
+            try {
+                newFigure.createTwoColorCreasePattern(creasePatternCamera, lines);
+            } catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
     }
 }
