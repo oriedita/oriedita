6940c2a57156f1fb9520faf6d169fb7af874b50a
Minh Than <hoangminh117@gmail.com>
2023-09-01 12:06:48 -0500

    Implement DXF exporting (with bug, currently looking into it)

---
+++ oriedita/src/main/java/oriedita/editor/export/Dxf.java
@@ -0,0 +1,100 @@
+package oriedita.editor.export;
+
+import org.tinylog.Logger;
+import oriedita.editor.save.Save;
+import origami.crease_pattern.element.LineColor;
+import origami.crease_pattern.element.LineSegment;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class Dxf {
+    public static void exportFile(Save cp, File file) {
+        double scale = 3.0;
+        double center = 4.0;
+        double x1, y1, x2, y2;
+        LineColor lineColor;
+
+        try(FileWriter fw = new FileWriter(file); BufferedWriter bw = new BufferedWriter(fw); PrintWriter pw = new PrintWriter(bw)){
+            pw.println("  0");
+            pw.println("SECTION");
+            pw.println("  2");
+            pw.println("HEADER");
+            pw.println("  9");
+            pw.println("$ACADVER");
+            pw.println("  1");
+            pw.println("AC1009");
+            pw.println("  0");
+            pw.println("ENDSEC");
+            pw.println("  0");
+            pw.println("SECTION");
+            pw.println("  2");
+            pw.println("ENTITIES");
+
+
+            for (LineSegment lineSegment : cp.getLineSegments()) {
+                    pw.println("  0");
+                    pw.println("LINE");
+                    pw.println("  8");
+
+                    lineColor = lineSegment.getColor();
+                    String layerName = "noname";
+                    int colorNumber = 0;
+
+                    switch (lineColor) {
+                        case BLACK_0:
+                            layerName = "CutLine";
+                            colorNumber = 250; // gray
+                            break;
+                        case RED_1:
+                            layerName = "MountainLine";
+                            colorNumber = 1; // red
+                            break;
+                        case BLUE_2:
+                            layerName = "ValleyLine";
+                            colorNumber = 5; // blue
+                            break;
+                        case CYAN_3:
+                            layerName = "AuxiliaryLine";
+                            colorNumber = 4; // cyan
+                    }
+                    x1 = lineSegment.determineAX();
+                    y1 = lineSegment.determineAY();
+                    x2 = lineSegment.determineBX();
+                    y2 = lineSegment.determineBY();
+
+                    pw.println(layerName);
+                    pw.println("  6");
+                    pw.println("CONTINUOUS");
+                    pw.println("  62");
+                    pw.println(colorNumber);
+
+                    pw.println("  10");
+                    pw.println(scale(x1, scale, center));
+                    pw.println("  20");
+                    pw.println(scale(y1, -scale, center));
+
+                    pw.println("  11");
+                    pw.println(scale(x2, scale, center));
+                    pw.println("  21");
+                    pw.println(scale(y2, -scale, center));
+
+            }
+
+            pw.println("  0\n");
+            pw.println("ENDSEC\n");
+            pw.println("  0\n");
+            pw.println("EOF\n");
+        } catch (IOException e) {
+            Logger.error(e, "Error during Dxf export");
+        }
+    }
+
+    private static double scale(double d, double scale, double center) {
+        return d * scale + center;
+    }
+}
