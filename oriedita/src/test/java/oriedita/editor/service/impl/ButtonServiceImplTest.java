008b59cf01208f84e7fc156c0d07f236a39565cc
Gerben Oolbekkink <qurben@users.noreply.github.com>
2024-03-02 15:22:09 +0100

    Provide ButtonServiceImplTest a dummy implementation
    Would prevent a NPE when not running in headless mode.

--- oriedita/src/test/java/oriedita/editor/service/impl/ButtonServiceImplTest.java
+++ oriedita/src/test/java/oriedita/editor/service/impl/ButtonServiceImplTest.java
@@ -1,8 +1,12 @@
 package oriedita.editor.service.impl;
 
 import oriedita.editor.FrameProviderImpl;
+import oriedita.editor.action.ActionService;
+import oriedita.editor.action.OrieditaAction;
 import oriedita.editor.service.ButtonService;
 import oriedita.editor.service.ButtonServiceTest;
+
+import java.util.List;
 
 public class ButtonServiceImplTest extends ButtonServiceTest {
 
@@ -10,8 +14,25 @@ public class ButtonServiceImplTest extends ButtonServiceTest {
     public ButtonService createInstance() {
         return new ButtonServiceImpl(
                 new FrameProviderImpl(),
-                null, null, null, null // these dependencies should only be used once a button
-                                                                // is clicked, which doesn't happen in the test
+                null, // these dependencies should only be used once a button
+                null, // is clicked, which doesn't happen in the test
+                null,
+                new TestActionService()
         );
+    }
+
+    /**
+     * Empty stub for test.
+     */
+    private static class TestActionService implements ActionService {
+        @Override
+        public void registerAction(OrieditaAction orieditaAction) {
+
+        }
+
+        @Override
+        public List<OrieditaAction> getAllRegisteredActions() {
+            return List.of();
+        }
     }
 }
