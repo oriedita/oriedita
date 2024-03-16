b5f2828d7455f75b7c50095821f381e366340506
Gerben Oolbekkink <qurben@users.noreply.github.com>
2024-03-16 10:39:54 +0100

    Move ActionType out of OrieditaAction

--- oriedita-ui/src/main/java/oriedita/editor/action/DegAction.java
+++ oriedita-ui/src/main/java/oriedita/editor/action/DegAction.java
@@ -11,20 +11,17 @@ public class DegAction extends AbstractOrieditaAction implements OrieditaAction{
     private final CanvasModel canvasModel;
     private final AngleSystemModel angleSystemModel;
     private final ButtonService buttonService;
-    private final ActionType actionType;
     private final MouseMode mouseMode;
     private final AngleSystemModel.AngleSystemInputType angleSystemInputType;
 
     public DegAction(CanvasModel canvasModel,
                      AngleSystemModel angleSystemModel,
                      ButtonService buttonService,
-                     ActionType actionType,
                      MouseMode mouseMode,
                      AngleSystemModel.AngleSystemInputType angleSystemInputType){
         this.canvasModel = canvasModel;
         this.angleSystemModel = angleSystemModel;
         this.buttonService = buttonService;
-        this.actionType = actionType;
         this.mouseMode = mouseMode;
         this.angleSystemInputType = angleSystemInputType;
     }
@@ -34,10 +31,5 @@ public class DegAction extends AbstractOrieditaAction implements OrieditaAction{
         angleSystemModel.setAngleSystemInputType(angleSystemInputType);
         canvasModel.setMouseMode(mouseMode);
         buttonService.Button_shared_operation();
-    }
-
-    @Override
-    public ActionType getActionType(){
-        return actionType;
     }
 }
--- oriedita-ui/src/main/java/oriedita/editor/action/OrieditaAction.java
+++ oriedita-ui/src/main/java/oriedita/editor/action/OrieditaAction.java
@@ -1,16 +1,6 @@
 package oriedita.editor.action;
 
 public interface OrieditaAction extends javax.swing.Action {
-    default ActionType getActionType() {
-        ActionHandler annotation = getClass().getAnnotation(ActionHandler.class);
-
-        if (annotation == null){
-            throw new IllegalStateException("Action does not have an @ActionHandler annotation: " + getClass());
-        }
-
-        return annotation.value();
-    }
-
     default boolean resetLineStep() {
         return true;
     }
