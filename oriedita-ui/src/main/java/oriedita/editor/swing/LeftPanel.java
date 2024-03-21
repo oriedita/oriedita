e248009797be89b2afb54787a9e3544bc26895f8
Minh Than <hoangminh117@gmail.com>
2024-03-21 11:47:42 -0500

    make popup menu show on scroll and snappier scrolling

--- oriedita-ui/src/main/java/oriedita/editor/swing/LeftPanel.java
+++ oriedita-ui/src/main/java/oriedita/editor/swing/LeftPanel.java
@@ -330,14 +330,16 @@ public class LeftPanel {
         delTypeDropBox.addMouseWheelListener(new MouseAdapter() {
             @Override
             public void mouseWheelMoved(MouseWheelEvent e) {
+
                 int index = delTypeDropBox.getSelectedIndex();
                 if (e.getWheelRotation() > 0 && index < delTypeDropBox.getItemCount() - 1) {
                     applicationModel.setDelLineType(CustomLineTypes.from(index));
-                    del_l_typeButton.doClick();
                 } else if (e.getWheelRotation() < 0 && index > 0) {
                     applicationModel.setDelLineType(CustomLineTypes.from(index - 2));
-                    del_l_typeButton.doClick();
                 }
+
+                e.consume();
+                delTypeDropBox.showPopup();
             }
         });
         delTypeDropBox.addPopupMenuListener(new PopupMenuAdapter() {
@@ -359,11 +361,12 @@ public class LeftPanel {
                 int index = fromLineDropBox.getSelectedIndex();
                 if (e.getWheelRotation() > 0 && index < fromLineDropBox.getItemCount() - 1) {
                     applicationModel.setCustomFromLineType(CustomLineTypes.from(index));
-                    replace_lineButton.doClick();
                 } else if (e.getWheelRotation() < 0 && index > 0) {
                     applicationModel.setCustomFromLineType(CustomLineTypes.from(index - 2));
-                    replace_lineButton.doClick();
                 }
+
+                e.consume();
+                fromLineDropBox.showPopup();
             }
         });
         fromLineDropBox.addPopupMenuListener(new PopupMenuAdapter() {
@@ -391,7 +394,6 @@ public class LeftPanel {
                         index++;
                         applicationModel.setCustomToLineType(CustomLineTypes.from(index + 1));
                     }
-                    replace_lineButton.doClick();
                 } else if (e.getWheelRotation() < 0 && index > 0) {
                     if (index + 1 == CustomLineTypes.MOUNTAIN.getNumber()) {
                         applicationModel.setCustomToLineType(CustomLineTypes.from(index - 2));
@@ -399,8 +401,10 @@ public class LeftPanel {
                         index++;
                         applicationModel.setCustomToLineType(CustomLineTypes.from(index - 1));
                     }
-                    replace_lineButton.doClick();
                 }
+
+                e.consume();
+                toLineDropBox.showPopup();
             }
         });
         toLineDropBox.addPopupMenuListener(new PopupMenuAdapter() {
