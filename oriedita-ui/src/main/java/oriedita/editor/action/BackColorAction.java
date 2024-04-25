c6c904ce00ed0bae22b50293e5d830602832ab2d
Minh Than <hoangminh117@gmail.com>
2024-04-25 02:15:32 -0500

    add CustomColorChooserPanel to BackColorAction and LineColorAction

--- oriedita-ui/src/main/java/oriedita/editor/action/BackColorAction.java
+++ oriedita-ui/src/main/java/oriedita/editor/action/BackColorAction.java
@@ -4,10 +4,13 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import oriedita.editor.FrameProvider;
 import oriedita.editor.databinding.FoldedFigureModel;
+import oriedita.editor.swing.CustomColorChooserPanel;
 
 import javax.swing.JColorChooser;
+import javax.swing.JDialog;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 @ApplicationScoped
 @ActionHandler(ActionType.backColorAction)
@@ -25,10 +28,29 @@ public class BackColorAction extends AbstractOrieditaAction {
     @Override
     public void actionPerformed(ActionEvent e) {
         //以下にやりたいことを書く
-        Color backColor = JColorChooser.showDialog(frameProvider.get(), "B_col", Color.white);
+        Color backColor = showCustomColorDialog(frameProvider, "B_col",foldedFigureModel.getBackColor());
 
         if (backColor != null) {
             foldedFigureModel.setBackColor(backColor);
         }
+    }
+
+    private Color showCustomColorDialog(FrameProvider frameProvider, String title, Color initialColor){
+        JColorChooser colorChooser = new JColorChooser();
+        colorChooser.addChooserPanel(new CustomColorChooserPanel());
+
+        final boolean[] isOK = new boolean[1];
+
+        if(initialColor != null){
+            colorChooser.setColor(initialColor);
+        }
+        ActionListener okListener = e -> isOK[0] = true;
+
+        ActionListener cancelListener = e -> isOK[0] = false;
+
+        JDialog dialog = JColorChooser.createDialog(frameProvider.get(), title, true, colorChooser, okListener, cancelListener);
+        dialog.setVisible(true);
+
+        return isOK[0] ? colorChooser.getColor() : initialColor;
     }
 }
--- oriedita-ui/src/main/java/oriedita/editor/action/LineColorAction.java
+++ oriedita-ui/src/main/java/oriedita/editor/action/LineColorAction.java
@@ -4,10 +4,13 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import oriedita.editor.FrameProvider;
 import oriedita.editor.databinding.FoldedFigureModel;
+import oriedita.editor.swing.CustomColorChooserPanel;
 
 import javax.swing.JColorChooser;
+import javax.swing.JDialog;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 @ApplicationScoped
 @ActionHandler(ActionType.lineColorAction)
@@ -26,9 +29,28 @@ public class LineColorAction extends AbstractOrieditaAction {
     public void actionPerformed(ActionEvent e) {
         //以下にやりたいことを書く
 
-        Color lineColor = JColorChooser.showDialog(frameProvider.get(), "L_col", Color.white);
+        Color lineColor = showCustomColorDialog(frameProvider, "L_col", foldedFigureModel.getLineColor());
         if (lineColor != null) {
             foldedFigureModel.setLineColor(lineColor);
         }
+    }
+
+    private Color showCustomColorDialog(FrameProvider frameProvider, String title, Color initialColor){
+        JColorChooser colorChooser = new JColorChooser();
+        colorChooser.addChooserPanel(new CustomColorChooserPanel());
+
+        final boolean[] isOK = new boolean[1];
+
+        if(initialColor != null){
+            colorChooser.setColor(initialColor);
+        }
+        ActionListener okListener = e -> isOK[0] = true;
+
+        ActionListener cancelListener = e -> isOK[0] = false;
+
+        JDialog dialog = JColorChooser.createDialog(frameProvider.get(), title, true, colorChooser, okListener, cancelListener);
+        dialog.setVisible(true);
+
+        return isOK[0] ? colorChooser.getColor() : initialColor;
     }
 }
