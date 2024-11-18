96975f485e27f4a00a490f70e81a53119312e915
Minh Than <hoangminh117@gmail.com>
2024-11-18 14:36:47 +0700

    close polygon upon completion + refactoring
    Signed-off-by: Minh Than <hoangminh117@gmail.com>

--- oriedita/src/main/java/oriedita/editor/handler/MouseHandlerSelectPolygon.java
+++ oriedita/src/main/java/oriedita/editor/handler/MouseHandlerSelectPolygon.java
@@ -3,12 +3,18 @@ package oriedita.editor.handler;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import oriedita.editor.canvas.MouseMode;
+import origami.crease_pattern.element.LineSegment;
+import origami.crease_pattern.element.Polygon;
 
 @ApplicationScoped
 @Handles(MouseMode.SELECT_POLYGON_66)
 public class MouseHandlerSelectPolygon extends BaseMouseHandlerPolygon {
     @Inject
-    public MouseHandlerSelectPolygon() {
-    }
+    public MouseHandlerSelectPolygon() {}
 
+    @Override
+    protected void performAction() {
+        Polygon polygon = new Polygon(d.getLineStep().stream().map(LineSegment::getA).toList());
+        d.getFoldLineSet().select_Takakukei(polygon, "select");
+    }
 }
