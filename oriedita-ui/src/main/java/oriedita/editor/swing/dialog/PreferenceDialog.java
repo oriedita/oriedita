bcf25955f73ebf60108e17239eabbc2751398817
Minh Than <hoangminh117@gmail.com>
2024-11-25 10:58:51 +0700

    tweak
    Signed-off-by: Minh Than <hoangminh117@gmail.com>

--- oriedita-ui/src/main/java/oriedita/editor/swing/dialog/PreferenceDialog.java
+++ oriedita-ui/src/main/java/oriedita/editor/swing/dialog/PreferenceDialog.java
@@ -598,7 +598,7 @@ public class PreferenceDialog extends JDialog {
     }
 
     private void setupCategoryPanel(JPanel listPanel, String categoryHeader) {
-        CollapsiblePanel categoryPanel = new CollapsiblePanel(categoryHeader.toUpperCase(), listPanel, new Insets(0, 0, 5, 0));
+        CollapsiblePanel categoryPanel = new CollapsiblePanel(categoryHeader.toUpperCase(), listPanel);
         hotkeyPanel.add(categoryPanel, new GridConstraints(categoryHeaderList.indexOf(categoryHeader), 0, 1, 1, GridConstraints.ANCHOR_NORTHWEST, GridConstraints.FILL_HORIZONTAL, 1, 1, null, null, null, 0, false));
     }
 
@@ -1384,7 +1384,7 @@ public class PreferenceDialog extends JDialog {
         contentPane.add(bottomPanel, BorderLayout.SOUTH);
         bottomPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEmptyBorder(), null, TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, null, null));
         final JPanel panel13 = new JPanel();
-        panel13.setLayout(new GridLayoutManager(1, 5, new Insets(0, 0, 0, 10), -1, -1));
+        panel13.setLayout(new GridLayoutManager(1, 5, new Insets(10, 0, 0, 10), -1, -1));
         bottomPanel.add(panel13, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_SOUTHEAST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         buttonOK = new JButton();
         buttonOK.setText("OK");
