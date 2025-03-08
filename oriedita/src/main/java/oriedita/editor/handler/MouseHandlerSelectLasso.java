94ee23cda6d8fca36e190225bb87c977fd1a31c4
Minh Than <hoangminh117@gmail.com>
2025-03-08 12:27:09 +0700

    fix: properly record upon tangible select/unselect lasso

--- oriedita/src/main/java/oriedita/editor/handler/MouseHandlerSelectLasso.java
+++ oriedita/src/main/java/oriedita/editor/handler/MouseHandlerSelectLasso.java
@@ -2,16 +2,24 @@ package oriedita.editor.handler;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import oriedita.editor.canvas.CreasePattern_Worker;
 import oriedita.editor.canvas.MouseMode;
 
 @ApplicationScoped
 @Handles(MouseMode.SELECT_LASSO_74)
 public class MouseHandlerSelectLasso extends BaseMouseHandlerLasso{
+    @Inject @Named("mainCreasePattern_Worker")
+    CreasePattern_Worker mainCreasePatternWorker;
+
     @Inject
     public MouseHandlerSelectLasso(){}
 
     @Override
     protected void performAction() {
+        int beforeSelectNum = mainCreasePatternWorker.getFoldLineTotalForSelectFolding();
         d.getFoldLineSet().select_lasso(d.getLinePath(), "select");
+        int afterSelectNum = mainCreasePatternWorker.getFoldLineTotalForSelectFolding();
+        if(beforeSelectNum != afterSelectNum) d.record();
     }
 }
--- oriedita/src/main/java/oriedita/editor/handler/MouseHandlerUnselectLasso.java
+++ oriedita/src/main/java/oriedita/editor/handler/MouseHandlerUnselectLasso.java
@@ -2,16 +2,24 @@ package oriedita.editor.handler;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import oriedita.editor.canvas.CreasePattern_Worker;
 import oriedita.editor.canvas.MouseMode;
 
 @ApplicationScoped
 @Handles(MouseMode.UNSELECT_LASSO_75)
 public class MouseHandlerUnselectLasso extends BaseMouseHandlerLasso{
+    @Inject @Named("mainCreasePattern_Worker")
+    CreasePattern_Worker mainCreasePatternWorker;
+
     @Inject
     public MouseHandlerUnselectLasso(){}
 
     @Override
     protected void performAction() {
+        int beforeSelectNum = mainCreasePatternWorker.getFoldLineTotalForSelectFolding();
         d.getFoldLineSet().select_lasso(d.getLinePath(), "unselect");
+        int afterSelectNum = mainCreasePatternWorker.getFoldLineTotalForSelectFolding();
+        if(beforeSelectNum != afterSelectNum) d.record();
     }
 }
