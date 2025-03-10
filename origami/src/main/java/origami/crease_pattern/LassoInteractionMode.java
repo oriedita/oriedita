faa76114b58c28e44e2737e32901af5224f1a4aa
Minh Than <hoangminh117@gmail.com>
2025-03-10 13:35:26 +0700

    chore: clean up enum

--- origami/src/main/java/origami/crease_pattern/LassoInteractionMode.java
+++ origami/src/main/java/origami/crease_pattern/LassoInteractionMode.java
@@ -1,12 +1,6 @@
 package origami.crease_pattern;
 
 public enum LassoInteractionMode {
-    INTERSECT(0),
-    CONTAIN(1);
-
-    private final int mode;
-
-    LassoInteractionMode(int mode) {
-        this.mode = mode;
-    }
+    INTERSECT,
+    CONTAIN;
 }
