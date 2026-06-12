6c5b938a93984419f4548c7bace2e522983ee2bc
undertrox <undertrox@gmail.com>
2026-06-12 20:42:27 +0200

    update change standard face handler

--- oriedita/src/main/java/oriedita/editor/handler/MouseHandlerChangeStandardFace.java
+++ oriedita/src/main/java/oriedita/editor/handler/MouseHandlerChangeStandardFace.java
@@ -4,18 +4,33 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import org.tinylog.Logger;
+import oriedita.editor.Colors;
 import oriedita.editor.canvas.CreasePattern_Worker;
 import oriedita.editor.canvas.MouseMode;
 import oriedita.editor.databinding.FoldedFiguresList;
 import oriedita.editor.drawing.FoldedFigure_Drawer;
+import oriedita.editor.drawing.tools.Camera;
+import oriedita.editor.handler.step.StepFactory;
+import oriedita.editor.handler.step.StepGraph;
+import oriedita.editor.handler.step.StepMouseHandler;
 import origami.crease_pattern.element.Point;
+import origami.crease_pattern.element.Polygon;
 import origami.folding.FoldedFigure;
+
+import java.awt.Graphics2D;
+import java.awt.geom.Path2D;
+import java.util.ArrayList;
+import java.util.List;
 
 @ApplicationScoped
 @Handles(MouseMode.CHANGE_STANDARD_FACE_103)
-public class MouseHandlerChangeStandardFace extends BaseMouseHandler {
-    private final CreasePattern_Worker d;
+public class MouseHandlerChangeStandardFace extends StepMouseHandler<MouseHandlerChangeStandardFace.Step> {
     private final FoldedFiguresList foldedFiguresList;
+    private Polygon polygon;
+
+    public enum Step {
+        SELECT_STARTING_FACE
+    }
 
     @Inject
     public MouseHandlerChangeStandardFace(FoldedFiguresList foldedFiguresList, @Named("mainCreasePattern_Worker") CreasePattern_Worker d) {
@@ -24,46 +39,83 @@ public class MouseHandlerChangeStandardFace extends BaseMouseHandler {
     }
 
     @Override
-    public void mouseMoved(Point p0) {
-
+    protected StepGraph<Step> initStepGraph(StepFactory stepFactory) {
+        var g = new StepGraph<>(Step.SELECT_STARTING_FACE);
+        g.addNode(stepFactory.createNode_MD_R(
+                Step.SELECT_STARTING_FACE,
+                p -> {
+                    FoldedFigure_Drawer selectedFigure = foldedFiguresList.getActiveItem();
+                    if (selectedFigure == null) {
+                        return;
+                    }
+                    var pointSet = selectedFigure.getFoldedFigure().wireFrameWorker_flatCp.get();
+                    int newStartingFaceId = pointSet.inside(p);
+                    if (newStartingFaceId < 1) return;
+                    var face = pointSet.getFace(newStartingFaceId);
+                    var numPoints = face.getNumPoints();
+                    List<Point> points = new ArrayList<>();
+                    for (int i = 1; i <= numPoints; i++) {
+                        points.add(pointSet.getPoint(face.getPointId(i)));
+                    }
+                    polygon = new Polygon(points);
+                },
+                p -> {
+                    FoldedFigure_Drawer selectedFigure = foldedFiguresList.getActiveItem();
+                    if (selectedFigure == null) {
+                        return Step.SELECT_STARTING_FACE;
+                    }
+                    changeStartingFace(selectedFigure, p);
+                    return Step.SELECT_STARTING_FACE;
+                }
+        ));
+        return g;
     }
 
     @Override
-    public void mousePressed(Point p0) {
+    public void drawPreview(Graphics2D g2, Camera camera, DrawingSettings settings) {
+        super.drawPreview(g2, camera, settings);
+        if (polygon == null) {return;}
+        g2.setColor(Colors.get(Colors.FIGURE_HIGHLIGHT));
+        Path2D.Double path = new Path2D.Double();
 
+        Point t1 = camera.object2TV(polygon.get(0));
+        path.moveTo(t1.getX(), t1.getY());
+
+        for (int i = 1; i < polygon.degree(); i++) {
+            t1 = camera.object2TV(polygon.get(i));
+            path.lineTo(t1.getX(), t1.getY());
+        }
+
+        path.closePath();
+
+        g2.fill(path);
     }
 
     @Override
-    public void mouseDragged(Point p0) {
-
+    public void reset() {
+        super.reset();
+        polygon = null;
     }
 
-    @Override
-    public void mouseReleased(Point p0) {
+    private static void changeStartingFace(FoldedFigure_Drawer selectedFigure, Point p) {
+        int oldStartingFaceId = selectedFigure.getStartingFaceId();
 
-        FoldedFigure_Drawer selectedFigure = foldedFiguresList.getActiveItem();
+        int newStartingFaceId = selectedFigure.getFoldedFigure().wireFrameWorker_flatCp.get().inside(p);
 
-        if (selectedFigure != null) {
-            Point p = d.getCamera().TV2object(p0);
-            int oldStartingFaceId = selectedFigure.getStartingFaceId();
+        if (newStartingFaceId < 1) return;
 
-            int newStartingFaceId = selectedFigure.getFoldedFigure().wireFrameWorker_flatCp.get().inside(p);
+        selectedFigure.setStartingFaceId(newStartingFaceId);
 
-            if (newStartingFaceId < 1) return;
+        Logger.info("kijyunmen_id = " + newStartingFaceId);
+        if (selectedFigure.getFoldedFigure().foldedFigure_worker.face_rating != null) {//20180227追加
+            int index = selectedFigure.getFoldedFigure().foldedFigure_worker.nbox.getSequence(newStartingFaceId);
+            Logger.info(
+                    "OZ.js.nbox.get_jyunjyo = " + index + " , rating = " + selectedFigure.getFoldedFigure().foldedFigure_worker.nbox.getWeight(index)
+            );
 
-            selectedFigure.setStartingFaceId(newStartingFaceId);
-
-            Logger.info("kijyunmen_id = " + newStartingFaceId);
-            if (selectedFigure.getFoldedFigure().foldedFigure_worker.face_rating != null) {//20180227追加
-                int index = selectedFigure.getFoldedFigure().foldedFigure_worker.nbox.getSequence(newStartingFaceId);
-                Logger.info(
-                        "OZ.js.nbox.get_jyunjyo = " + index + " , rating = " + selectedFigure.getFoldedFigure().foldedFigure_worker.nbox.getWeight(index)
-                );
-
-            }
-            if ((newStartingFaceId != oldStartingFaceId) && (selectedFigure.getFoldedFigure().estimationStep != FoldedFigure.EstimationStep.STEP_0)) {
-                selectedFigure.getFoldedFigure().estimationStep = FoldedFigure.EstimationStep.STEP_1;
-            }
+        }
+        if ((newStartingFaceId != oldStartingFaceId) && (selectedFigure.getFoldedFigure().estimationStep != FoldedFigure.EstimationStep.STEP_0)) {
+            selectedFigure.getFoldedFigure().estimationStep = FoldedFigure.EstimationStep.STEP_1;
         }
     }
 }
